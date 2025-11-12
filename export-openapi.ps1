ce332464817230b70cf07833ff097cb78fce37ec
ueslim <107502335+ueslim@users.noreply.github.com>
2025-11-12 17:29:23 -0300

    limpeza para migração dos microserviços

--- export-openapi.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [string]$UsersBase = "http://localhost:5001",
-  [string]$GamesBase = "http://localhost:5002",
-  [string]$PaymentsBase = "http://localhost:5003"
-)
-
-Invoke-WebRequest "$UsersBase/swagger/v1/swagger.json" -OutFile "openapi.users.json"
-Invoke-WebRequest "$GamesBase/swagger/v1/swagger.json" -OutFile "openapi.games.json"
-Invoke-WebRequest "$PaymentsBase/swagger/v1/swagger.json" -OutFile "openapi.payments.json"
